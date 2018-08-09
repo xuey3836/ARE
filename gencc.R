71314f714686d6591684ae787c3ee337575b06c7
xuey3836 <xuey3836@outlook.com>
2018-08-09 09:55:37 +0800

    sss

--- gencc.R
+++ gencc.R
@@ -2,11 +2,11 @@
 gencc = function(r,s,k,maf,lambda,theta){
   lambda1 = lambda
   lambda2 = 1- theta+theta*lambda1
-  g = c((1-maf)^2,2*maf*(1-maf),maf^2)
+  g = c(maf^2,2*maf*(1-maf),(1-maf)^2)
   f = rep(0,3)
-  f[3] = k/(lambda2*g[1]+lambda1*g[2]+g[3])
-  f[2] = lambda1*f[3]
-  f[1]= lambda2*f[3]
+  f[3] = k/(lambda1*g[1]+lambda2*g[2]+g[3])
+  f[2] = lambda2*f[3]
+  f[1]= lambda1*f[3]
   p = g*f/k
   q = g*(1-f)/(1-k)
   rv = rowSums(rmultinom(r,1,prob = p))
--- sim.R
+++ sim.R
@@ -1,6 +1,6 @@
 rm(list = ls())
-# setwd("C:/Users/o0/Desktop/ARE/")
-setwd("/home/o0/Desktop/ARE/")
+setwd("C:/Users/o0/Desktop/ARE/")
+# setwd("/home/o0/Desktop/ARE/")
 source("loglik.R")
 source("gencc.R")
 
@@ -10,9 +10,9 @@ s=  500
 k = 0.05
 # eta= log(1-1/(exp(k)))
 maf = 0.35
-lambda = 1.5
+lambda = 1
 thetav = c(0,1/4,1/2,1)
-theta= thetav[4]
+theta= thetav[3]
 Nrep= 100
 result = matrix(0,4,Nrep)
 for (t in 1:Nrep){
@@ -34,57 +34,64 @@ for (t in 1:Nrep){
   }
   
   ##s(theta,eta)
-  mu_theta_eta = function(theta,eta,mat){
+  mu_theta_eta = function(theta,eta,mat,lambda){
     n = sum(mat)
     m = length(eta)
-    l1 = cgradLik(c(1,1,eta),mat)
+    mat1 = gencc(r,s,k,maf,lambda,theta)
+    l1 = cgradLik(c(1,1,eta),mat1)
     L <- chessianLik(c(1,1,eta),mat)/n
+    L1 <- chessianLik(c(1,1,eta),mat1)/n
     s1 = l1[1] + theta*l1[2]
     s2 =(L[1,3]+theta*L[2,3])%*%solve(L[3,3])
     s = s1 - s2%*%l1[3]
     mu = s/n
-    mu_1 = (L[1,1]+theta*L[1,2]-s2%*%L[1,3])
+    mu_1 = (L1[1,1]+theta*L1[1,2]-s2%*%L1[1,3])
     return(list(mu=as.numeric(mu),mu_1=as.numeric(mu_1)))
   }
   ##
   
-  
+  st00 = sigma_theta(L,theta,theta)
+  st0i =sigma_theta(L,thetai,theta)
+  st0j= sigma_theta(L,thetaj,theta)
+  stii=sigma_theta(L,thetai,thetai)
+  stjj=sigma_theta(L,thetaj,thetaj)
+  stij= sigma_theta(L,thetai,thetaj)
   ##Pitman ARE
-  rho_theta0i= sigma_theta(L,theta,thetai)/sqrt(sigma_theta(L,theta,theta)*sigma_theta(L,thetai,thetai))
-  rho_theta0j= sigma_theta(L,theta,thetaj)/sqrt(sigma_theta(L,theta,theta)*sigma_theta(L,thetaj,thetaj))
-  rho_thetaij= sigma_theta(L,thetai,thetaj)/sqrt((sigma_theta(L,thetai,thetai)*sigma_theta(L,thetaj,thetaj)))
+  rho_theta0i= st0i/sqrt(st00*stii)
+  rho_theta0j= st0j/sqrt(st00*stjj)
+  rho_thetaij= stij/sqrt(stii*stjj)
   Ep_MZ= (rho_theta0i+rho_theta0j)^2/(2*(1+rho_thetaij))
   
-  mu11= mu_theta_eta(theta,eta,matdat)
-  mu1i = mu_theta_eta(thetai,eta,matdat)
-  mu1j = mu_theta_eta(thetaj,eta,matdat)
-  mu = mu11$mu
-  mu_i= mu1i$mu
-  mu_j = mu1j$mu
-  mu_1 = mu11$mu_1
-  mu_1i = mu1i$mu_1
-  mu_1j = mu1j$mu_1
+ 
+  mu_1 = mu_theta_eta(theta,eta,matdat,lambda=1)$mu_1
+  mu_1i = mu_theta_eta(theta,eta,matdat,lambda=1)$mu_1
+  mu_1j = mu_theta_eta(theta,eta,matdat,lambda=1)$mu_1
   ##Chernoff ARE
-  Q_Z = 2*(1- pnorm(mu_1/(2*sigma_theta(L,theta,theta))))
-  Q_M =2*(1- pnorm((mu_1i/(2*sigma_theta(L,thetai,thetai))+mu_1j/(2*sigma_theta(L,thetaj,thetaj)))/
+  Q_Z = 2*(1- pnorm(mu_1/(2*st00)))
+  Q_M =2*(1- pnorm((mu_1i/(2*stii)+mu_1j/(2*stjj))/
                      (sqrt(8*(1+rho_thetaij)))))
   Ec_MZ = Q_M/Q_Z
   
   ##Hodges-Ledman ARE
-  d_Z = (mu/sigma_theta(L,theta,theta))^2
-  d_M =((mu_i/sigma_theta(L,thetai,thetai)+mu_j/sigma_theta(L,thetaj,thetaj))/sqrt(2*(1+rho_thetaij)))^2
+  mu11= mu_theta_eta(theta,eta,matdat,lambda=1.5)
+  mu1i = mu_theta_eta(thetai,eta,matdat,lambda=1.5)
+  mu1j = mu_theta_eta(thetaj,eta,matdat,lambda=1.5)
+  mu = mu11$mu
+  mu_i= mu1i$mu
+  mu_j = mu1j$mu
+  d_Z = (mu/st00)^2
+  d_M =((mu_i/stii+mu_j/stjj)/sqrt(2*(1+rho_thetaij)))^2
   Ehl_MZ = d_M/d_Z
   
   ##Bahadur ARE
-  mu_1M = (mu_1i/sigma_theta(L,thetai,thetai)+  mu_1j/sigma_theta(L,thetaj,thetaj))/sqrt(2*(1+rho_thetaij))
-  c_Z = 1- pnorm(mu_1/sigma_theta(L,theta,theta))
+  mu_1M = (mu_1i/stii+  mu_1j/stjj)/sqrt(2*(1+rho_thetaij))
+  c_Z = 1- pnorm(mu_1/st00)
   c_M = 1- pnorm(mu_1M)
   Eb_MZ = c_M/c_Z
   
   result[,t]= c(Ep_MZ,Ec_MZ,Ehl_MZ,Eb_MZ)
-  
-  
 }
 
 rowMeans(result)
 apply(result,1,sd)
+boxplot(t(result),ylim=c(-1,10))
